cfe4a95a11d9d111f4dcbab0c568d8a20668dcf5
petruki <31597636+petruki@users.noreply.github.com>
2024-06-22 22:52:22 -0700

    Added goroutine StartAccountHandler skeleton exec plan

--- src/core/handler.go
+++ src/core/handler.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"sync"
+	"time"
+
 	"github.com/switcherapi/switcher-gitops/src/model"
 	"github.com/switcherapi/switcher-gitops/src/repository"
 )
@@ -20,9 +23,9 @@ func (c *CoreHandler) InitCoreHandlerCoroutine() (int, error) {
 	// Load all accounts
 	accounts, _ := c.AccountRepository.FetchAllActiveAccounts()
 
-	// Iterate over accounts and start a new account handler
+	// Iterate over accounts and start account handlers
 	for _, account := range accounts {
-		c.startNewAccountHandler(account)
+		go c.StartAccountHandler(account, make(chan bool), &sync.WaitGroup{})
 	}
 
 	// Update core handler status
@@ -30,6 +33,37 @@ func (c *CoreHandler) InitCoreHandlerCoroutine() (int, error) {
 	return c.status, nil
 }
 
-func (c *CoreHandler) startNewAccountHandler(account model.Account) {
-	c.AccountRepository.FetchByDomainId(account.Domain.ID)
+func (c *CoreHandler) StartAccountHandler(account model.Account, quit chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Check if account is not active
+	if !account.Settings.Active {
+		return
+	}
+
+	// Reads Window setting
+	sleep := 1
+
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+			lastCommit := "123"
+			date := time.Now().Format("2006-01-02 15:04:05")
+
+			if account.Domain.LastCommit == "" || account.Domain.LastCommit != lastCommit {
+				c.CheckForChanges(account, lastCommit, date)
+			}
+		}
+
+		time.Sleep(time.Duration(sleep) * time.Second)
+	}
+}
+
+func (c *CoreHandler) CheckForChanges(account model.Account, lastCommit string, date string) {
+	account.Domain.LastCommit = lastCommit
+	account.Domain.Status = "Synced"
+	account.Domain.Message = "Synced successfully"
+	c.AccountRepository.Update(&account)
 }
